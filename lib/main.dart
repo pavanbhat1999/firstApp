795321d54ae44f495093be1ae018a2ac7ae36cfc
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2020-08-23 09:54:15 +0530

    my next commit

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Hello I wrote My first flutter app'),
+      home: MyHomePage(title: 'Hello World I wrote My first flutter app'),
     );
   }
 }
